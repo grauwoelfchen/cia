e321452e7db2e7f513d32157da615b73b3958cd4
grauwoelfchen <y.grauwoelfchen@gmail.com>
2011-02-09 11:40:38 +0900

    added current ip text as contents.

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
+require 'sass'
 
 configure :production do
   # Configure stuff here you'll want to
@@ -7,8 +9,29 @@ configure :production do
 
   # TIP:  You can get you database information
   #       from ENV['DATABASE_URI'] (see /env route below)
+  error do
+    'Error'
+  end
+
+  not_found do
+    'Not Found'
+  end
 end
 
+configure :development do |c|
+  require 'sinatra/reloader'
+  c.also_reload '*.rb'
+
+  after do
+    p response.status
+  end
+end
+
+
 get '/' do
-  'Test'
+  haml :index, :locals => {:ip => request.ip}
+end
+
+get '/stylesheet.css' do
+  sass :stylesheet, :style => :compressed
 end
--- config.ru
+++ config.ru
@@ -1,5 +1,13 @@
-require 'app'
+require 'rubygems'
+require 'sinatra'
+use Rack::Lint
+
+Sinatra::Base.set(:views, File.join(File.dirname(__FILE__), 'views'))
+Sinatra::Base.set(:run,   false)
+Sinatra::Base.set(:env,   ENV['RACK_ENV'])
+
+app_dir = File.expand_path(File.dirname(__FILE__))
+require app_dir + '/app'
 
 set :environment, :production
-
 run Sinatra::Application
